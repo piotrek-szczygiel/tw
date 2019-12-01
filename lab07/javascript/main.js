4d1e236f59840d5b3816ecf19245a0c6cc39cfd9
Piotr Szczygieł <szczygiel.p@gmail.com>
2019-12-01 23:00:44 +0100

    Add lab07: javascript results

--- lab07/javascript/main.js
+++ lab07/javascript/main.js
@@ -1,3 +1,6 @@
+const { performance } = require("perf_hooks");
+const fs = require("fs");
+
 function timeout(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -50,6 +53,22 @@ class Fork {
     }
   }
 
+  async acquireWith(other) {
+    let wait = 1;
+
+    while (true) {
+      await timeout(wait);
+
+      if (this.taken === false && other.taken === false) {
+        this.taken = true;
+        other.taken = true;
+        break;
+      }
+
+      wait *= 2;
+    }
+  }
+
   release() {
     this.taken = false;
   }
@@ -62,14 +81,18 @@ class Philosopher {
     this.f1 = id % forks.length;
     this.f2 = (id + 1) % forks.length;
 
+    this.timesAsym = [];
+    this.timesConductor = [];
+    this.timesBoth = [];
+
     return this;
   }
 
   async startNaive(count) {
-    const forks = this.forks,
-      f1 = this.f1,
-      f2 = this.f2,
-      id = this.id;
+    const forks = this.forks;
+    const f1 = this.f1;
+    const f2 = this.f2;
+    const id = this.id;
 
     for (let i = 0; i < count; ++i) {
       console.log(`Philosopher ${id} awaiting first fork...`);
@@ -96,6 +119,8 @@ class Philosopher {
     const id = this.id;
 
     for (let i = 0; i < count; ++i) {
+      const start = performance.now();
+
       if (id % 2 === 0) {
         console.log(`Philosopher ${id} awaiting first fork...`);
         await forks[f1].acquire();
@@ -109,6 +134,8 @@ class Philosopher {
         console.log(`Philosopher ${id} awaiting first fork...`);
         await forks[f1].acquire();
       }
+
+      this.timesAsym.push(performance.now() - start);
 
       console.log(`Philosopher ${id} is eating...`);
       await timeout(Math.random() * 1000);
@@ -128,6 +155,8 @@ class Philosopher {
     const id = this.id;
 
     for (let i = 0; i < count; ++i) {
+      const start = performance.now();
+
       await conductor.lock();
 
       if (id % 2 === 0) {
@@ -144,6 +173,8 @@ class Philosopher {
         await forks[f1].acquire();
       }
 
+      this.timesConductor.push(performance.now() - start);
+
       console.log(`Philosopher ${id} is eating...`);
       await timeout(Math.random() * 1000);
 
@@ -156,45 +187,132 @@ class Philosopher {
       await timeout(Math.random() * 1000);
     }
   }
-}
 
-function runNaive(philosophers) {
-  console.log("\n\nNaive version");
+  async startBoth(count) {
+    const forks = this.forks;
+    const f1 = this.f1;
+    const f2 = this.f2;
+    const id = this.id;
 
-  for (let p of philosophers) {
-    p.startNaive(10);
+    for (let i = 0; i < count; ++i) {
+      const start = performance.now();
+
+      console.log(`Philosopher ${id} awaiting both forks...`);
+      await forks[f1].acquireWith(forks[f2]);
+
+      this.timesBoth.push(performance.now() - start);
+
+      console.log(`Philosopher ${id} is eating...`);
+      await timeout(Math.random() * 1000);
+
+      forks[f1].release();
+      forks[f2].release();
+
+      console.log(`Philosopher ${id} finished eating.`);
+      await timeout(Math.random() * 1000);
+    }
   }
 }
 
-function runAsym(philosophers) {
+async function runNaive(philosophers, iterations) {
+  console.log("\n\nNaive version");
+  let promises = [];
+
+  for (let p of philosophers) {
+    promises.push(p.startNaive(iterations));
+  }
+
+  for (let p of promises) {
+    await p;
+  }
+}
+
+async function runAsym(philosophers, iterations) {
   console.log("\n\nAsymmetrical version");
 
+  let promises = [];
   for (let p of philosophers) {
-    p.startAsym(10);
+    promises.push(p.startAsym(iterations));
+  }
+
+  for (let p of promises) {
+    await p;
   }
 }
 
-function runConductor(philosophers) {
+async function runConductor(philosophers, n, iterations) {
   console.log("\n\nConductor version");
 
-  let conductor = new Conductor(N);
+  let promises = [];
+  let conductor = new Conductor(n);
   for (let p of philosophers) {
-    p.startConductor(10, conductor);
+    promises.push(p.startConductor(iterations, conductor));
+  }
+
+  for (let p of promises) {
+    await p;
   }
 }
 
-const N = 5;
-let forks = [];
-let philosophers = [];
+async function runBoth(philosophers, iterations) {
+  console.log("\n\nBoth at once version");
 
-for (let i = 0; i < N; ++i) {
-  forks.push(new Fork());
+  let promises = [];
+  for (let p of philosophers) {
+    promises.push(p.startBoth(iterations));
+  }
+
+  for (let p of promises) {
+    await p;
+  }
 }
 
-for (let i = 0; i < N; ++i) {
-  philosophers.push(new Philosopher(i, forks));
+async function run() {
+  const iterations = 5;
+
+  for (let n of [5, 20, 100]) {
+    let forks = [];
+    let philosophers = [];
+
+    for (let i = 0; i < n; ++i) {
+      forks.push(new Fork());
+    }
+
+    for (let i = 0; i < n; ++i) {
+      philosophers.push(new Philosopher(i, forks));
+    }
+
+    await runConductor(philosophers, n, iterations);
+    await runAsym(philosophers, iterations);
+    await runBoth(philosophers, iterations);
+
+    let timesAsym = [];
+    let timesConductor = [];
+    let timesBoth = [];
+
+    for (let p of philosophers) {
+      timesAsym.push(...p.timesAsym);
+      timesConductor.push(...p.timesConductor);
+      timesBoth.push(...p.timesBoth);
+    }
+
+    const avg = arr => arr.reduce((a, b) => a + b, 0) / arr.length;
+
+    let results = `
+      Asym: ${avg(timesAsym)}
+      Conductor: ${avg(timesConductor)}
+      Both: ${avg(timesBoth)}
+    `;
+
+    console.info("Results:");
+    console.log(results);
+
+    fs.writeFile(`philosophers_${n}.txt`, results, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
 }
 
-// runNaive(philosophers);
-// runAsym(philosophers);
-// runConductor(philosophers);
+run();
